751a06fe3084c3849ee46b6c3ceae436407b4443
Jeffrey Bedoya <jjbedoya@sena.edu.co>
2021-09-25 01:35:25 -0500

    fin

---
+++ modulosA/app1.py
@@ -0,0 +1,4 @@
+import mimodulo
+print("la suma es ", mimodulo.suma(3,4))
+mimodulo.saludo()
+mimodulo.cuadrado_cubo(4)
---
+++ modulosA/mimodulo.py
@@ -0,0 +1,9 @@
+def suma(a,b):
+    return a+b
+
+def saludo():
+    print("Hola, has invocado unafuncion desde otro archivo")
+    return
+def cuadrado_cubo(a):
+    print("Has invocado funcion que pasa un solo parametro")
+    return a**2, a**3
